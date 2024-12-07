fcb461bf075057b43f2eca93a509c87036535bb5
xiesongjian <xiesongjian@dataeye.com>
2024-12-07 17:50:16 +0800

    refactor: 重命名DOMParser为Parser并更新引用

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './htmlparser2'
-import './DOMParser'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "./htmlparser2";
+import "./Parser";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
